2474280a810f47f19c42441dd385087f596cff30
aluparsad <84087576+aluparsad@users.noreply.github.com>
2022-09-30 12:01:53 +0530

    implemented refresh token

--- server2.js
+++ server2.js
@@ -11,6 +11,81 @@ const app = express();
 const users = []
 app.use(bodyParser.json())
 
+let refreshTokens = []
+
+
+app.post('/users/register', async(req,res)=>{
+    if(!hasEnoughInfo(req)) return res.sendStatus(406);
+    
+    if(userAlreadyExists(req)) return res.status(403).send("User Already Exists");
+
+    try{
+        const u = getHashedUser(extractUserInfoFromReq(req))
+        users.push(u)
+        res.status(201).send(`User Registered`)
+     }
+     catch(e){  
+        console.log(e)
+        res.status(500).send("Failed")
+     }
+})
+
+
+
+app.post('/users/login', (req,res)=>{
+    const user = users.find(user => user.name == req.body.name )
+
+    if(!user) return res.status(400).send("User doesn't Exists");
+
+    try{
+        
+        if((compareHash(req.body.password, user.salt, user.hash)))
+        {
+            return res.json(getAccessToken(user.email))
+        }
+
+        return res.status(404).json({err:"Wrong Password"})
+    }
+
+    catch(e){
+        res.status(500).send()
+    }
+})
+
+
+app.post('/token', (req,res)=>{
+    const refreshToken = req.body.token
+    if( refreshToken && !tokenExists(refreshToken) ) return res.sendStatus(401)
+
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if(err) return res.sendStatus(401)
+        
+        return res.json(getAccessToken(user.email))
+    })
+
+})
+
+
+app.delete('/logout',(req,res)=>{
+    const tkn = req.body.token;
+    if(!tkn) return res.sendStatus(401)
+    refreshTokens = refreshTokens.filter(token => token != tkn);
+    res.sendStatus(204);
+})
+
+app.get('/data', authenticateToken, (req,res)=>{
+    const u = users.find(user => user.email === req.user.email);
+    const reqUsr = req.user;
+    res.send([reqUsr, u] )
+})
+
+
+function userExists(email){
+    const usr = users.find(u => u.email === email) 
+    return usr != null
+}
+
+
 function getHashedUser (user){
     const salt = crypto.randomBytes(16).toString('hex')
     const hash = crypto.pbkdf2Sync(user.password, salt, 10000, 512, 'sha512').toString('hex');
@@ -31,54 +106,7 @@ function compareHash(password, salt, validHash){
 
 }
 
-app.get('/users',async (req,res)=>{
-    res.json(users)
-})
-
-app.post('/users/register', async(req,res)=>{
-    if(!hasEnoughInfo(req)) return res.sendStatus(406);
-    
-    if(userAlreadyExists(req)) return res.status(403).send("User Already Exists");
-
-    try{
-        const u = getHashedUser(extractUserInfoFromReq(req))
-        users.push(u)
-        res.status(201).send(`User Registered`)
-     }
-     catch(e){
-        console.log(e)
-        res.status(500).send("Failed")
-     }
-})
-
-
-
-
-app.post('/users/login', (req,res)=>{
-    const user = users.find(user => user.name == req.body.name )
-
-    if(!user) return res.status(400).send("User doesn't Exists");
-
-    try{
-        
-        if((compareHash(req.body.password, user.salt, user.hash)))
-            return res.json(getAccessToken(user.name))
-
-        return res.status(404).json({err:"Wrong Password"})
-    }
-    catch(e){
-        res.status(500).send()
-    }
-})
-
-
-app.get('/data', authenticateToken, (req,res)=>{
-    res.json(users.filter(user => user.username === req.user.username))
-})
-
-
-
-function extractUserInfoFromReq(req){
+function extractUserInfoFromReq(req){               
     const u = {
         name:req.body.name,
         username:req.body.username,
@@ -89,10 +117,12 @@ function extractUserInfoFromReq(req){
 }
 
 
-function getAccessToken(name){
-    const u = {name:name};
-    const accessToken = jwt.sign(u, process.env.ACCESS_TOKEN_SECRET)
-    return {accessToken:accessToken};
+function getAccessToken(email){
+    const user = {email:email};
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '30s'})
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+    refreshTokens.push(refreshToken);
+    return {accessToken:accessToken, refreshToken:refreshToken};
 }
 
 
@@ -103,7 +133,7 @@ function authenticateToken(req,res,next){
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user)=>{
         if(err) return res.sendStatus(403)
-
+        if(! userExists(user.email) ) return res.sendStatus(403)
         req.user = user;
         next();
     })
@@ -118,7 +148,12 @@ function userAlreadyExists(req){
     return u != null;
 }
 
-app.listen(PORT,()=>console.log(`server started`))
+function tokenExists(token){
+    const t = refreshTokens.find(tkn => tkn === token)
+    return t != null
+}
+
+app.listen(PORT,()=>console.log(`server started on port: ${PORT}`))
 
 
 
